1bd9570ecf71bfcf288f88d393ad6a13ef70f199
pavelborisov <pavel.borisofff@gmail.com>
2021-02-20 01:08:08 +0300

    swith class by time

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,30 @@
 'use strict';
 
+// generate current time format like '22:14'
 function startTime() {
     var today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
-    // var s = today.getSeconds();
+    var s = today.getSeconds();
     h = checkTime(h);
     m = checkTime(m);
-    // s = checkTime(s);
-    // const addText = checkSource(s);
-    document.getElementById('clock').innerHTML =
-    // h + ":" + m + ":" + s + addText;
-    h + ":" + m;
-    var t = setTimeout(startTime, 500);
+    const addText = checkSource(s);
+
+    // change clock's time
+    document.getElementById('clock').innerHTML = h + ":" + m;
+
+    // switch weather-avalanche
+    const weather = document.getElementsByClassName('weather')[0];
+    if (s < 30) {
+        weather.classList.add('visually-hidden');
+    } else {
+        weather.classList.remove('visually-hidden');
+    }
+    
+    var t = setTimeout(startTime, 1000);
 }
 
+// check current time's secconds to switch information blocks
 function checkSource(i) {
     let txt = '';
     if (i < 30) {
@@ -25,9 +35,11 @@ function checkSource(i) {
     return txt;
 }
 
+//visually-hidden
+// add zero in front of numbers < 10
 function checkTime(i) {
     if (i < 10) {
         i = "0" + i;
-    }  // add zero in front of numbers < 10
+    }
     return i;
 }
